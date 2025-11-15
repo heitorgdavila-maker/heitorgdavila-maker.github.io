2143174a9d811a3c59626e4887d3455bb1906266
heitorgdavila-maker <heitor.g.davila@gmail.com>
2025-11-16 01:21:12 +1000

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'tmg-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  event.respondWith(
+    caches.match(req).then(resp => {
+      if (resp) return resp;
+      return fetch(req).catch(() => caches.match('./index.html'));
+    })
+  );
+});
